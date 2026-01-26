a63f592060fe2093a2ec1386355cfcbabaf43387
Darshil chaudhry <darshilvinodbhai@gmail.com>
2026-01-26 21:10:02 +0530

    Added new files

---
+++ Arrys's/labwork8.2 Q-3.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main() {
+	int i,j,row,column,sum=0;
+	
+	printf("Enter the arrys row & column size:");
+	scanf("%d %d",&row,&column);
+	
+	int a[row][column];
+	printf("\nEnter arry's element:");
+	
+	for(i=0;i<row;i++){
+	  for(j=0;j<column;j++){
+	  	printf("\na[%d][%d]:",i,j);
+	  	scanf("%d",&a[i][j]);
+	  	
+	  	if(i+j == row-1){
+	 	sum += a[i][j];
+	    }
+	  }
+	}
+	 printf("\nthe sum of diagonal element of an arry:%d",sum);
+	 
+	return 0;
+}
